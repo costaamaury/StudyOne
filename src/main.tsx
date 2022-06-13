0f8ee3cd478689de3caf6a4419fac01f575921b3
costaamaury <56140348+costaamaury@users.noreply.github.com>
2022-06-13 15:21:51 -0300

    tela de login com campos e css simples

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import LoginPage from './pages/login'
+import HomePage from './pages/home'
+import './css/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
